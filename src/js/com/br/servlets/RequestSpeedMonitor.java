cca85601960404a087675a78803ad72e9f913893
João Salomão <joaosalomao.613@gmail.com>
2021-03-14 01:54:22 -0300

    Adicionando monitor do tempo de execução das requisições

---
+++ src/js/com/br/servlets/RequestSpeedMonitor.java
@@ -0,0 +1,27 @@
+package js.com.br.servlets;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletResponse;
+import javax.servlet.ServletRequest;
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter("/entry-point")
+public class RequestSpeedMonitor implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		long before = System.currentTimeMillis();
+		chain.doFilter(request, response);
+		long after = System.currentTimeMillis();
+
+		System.out.println("Action: " + request.getParameter("action"));
+		System.out.println("Execution time: " + (after - before) + "ms");
+
+	}
+
+}
